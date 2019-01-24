33f0f49da51e0829edcd351c518ed11027d1933d
iamishang <iamishan9@gmail.com>
2019-01-24 06:45:55 +0545

    Updates

---
+++ precache-manifest.87c59bd2a7af6f7f398779202e267298.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceabb18a006a6a79022a4363f8442351",
+    "url": "/hacker-news/static/media/bariol_regular-webfont.ceabb18a.woff"
+  },
+  {
+    "revision": "c0df7400b35639409f82383973bad884",
+    "url": "/hacker-news/static/media/bariol_regular-webfont.c0df7400.eot"
+  },
+  {
+    "revision": "ac437ca8426dbd7386bffda59c59a599",
+    "url": "/hacker-news/static/media/bariol_regular-webfont.ac437ca8.ttf"
+  },
+  {
+    "revision": "90c21e321caae8eecf5dc8c26318cc5f",
+    "url": "/hacker-news/static/media/bariol_regular-webfont.90c21e32.woff2"
+  },
+  {
+    "revision": "a4afb314ad17d5dbda6b",
+    "url": "/hacker-news/static/js/runtime~main.a4afb314.js"
+  },
+  {
+    "revision": "7a08e3540c18836882b2",
+    "url": "/hacker-news/static/js/main.7a08e354.chunk.js"
+  },
+  {
+    "revision": "8b121f31f40d9837208f",
+    "url": "/hacker-news/static/js/1.8b121f31.chunk.js"
+  },
+  {
+    "revision": "7a08e3540c18836882b2",
+    "url": "/hacker-news/static/css/main.d62bc16d.chunk.css"
+  },
+  {
+    "revision": "84537b5242ef41f67b488d7f8d5beeb3",
+    "url": "/hacker-news/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hacker-news/precache-manifest.87c59bd2a7af6f7f398779202e267298.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hacker-news/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
